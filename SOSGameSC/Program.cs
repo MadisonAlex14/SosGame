f9af0aa90633f34a2691da8266b0c1c8d6a86df1
Madison Alexander <159088794+MadisonAlex14@users.noreply.github.com>
2025-11-03 14:29:22 -0600

    Update Program.cs
    Updated after sprint 3

--- SOSGameSC/Program.cs
+++ SOSGameSC/Program.cs
@@ -1,18 +1,14 @@
 using System;
 using System.Windows.Forms;
-
 namespace SOSGameApp
 {
-    static class Program
+    internal static class Program
     {
-        // Main entry point
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            // Run the main form
             Application.Run(new Form1());
         }
     }
